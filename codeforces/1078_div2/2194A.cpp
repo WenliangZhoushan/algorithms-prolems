8b72471b4d52362c0a3d3d6cad49226c67dee927
亮亮 <wmz5132@psu.edu>
2026-02-09 00:49:59 -0500

    update code

---
+++ codeforces/1078_div2/2194A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+int n, w;
+
+void solve(int n, int w) {
+    int ans = 0;
+    ans += n / w * (w - 1);
+    ans += n % w;
+    cout << ans << endl;
+}
+
+int main() {
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        cin >> n >> w;
+        solve(n, w);
+    }
+}
---
+++ codeforces/1078_div2/2194B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, n;
+long long a[200010];
+int x, y;
+long long cnt[200010];
+
+void solve(int n, int x, int y) {
+    long long s = 0;
+    for (int i = 0; i < n; ++i) {
+        cin >> a[i];
+        cnt[i] = a[i] / x * y;
+        s += cnt[i];
+    }
+    long long ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans = max(ans, s - cnt[i] + a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main() {
+    cin >> t;
+    for (int i = 0; i < t; ++i) {
+        cin >> n >> x >> y;
+        solve(n, x, y);
+    }
+}
