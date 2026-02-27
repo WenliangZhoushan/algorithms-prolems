e0799f7b2ff41553eaf7cde186cc4aff053dcc88
亮亮 <wmz5132@psu.edu>
2026-02-27 00:12:30 -0500

    update code

---
+++ codeforces/762A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << "{"; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << "}"; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define debug_arr(a, n) do { cerr << #a << " = ["; for (int i = 0; i < (n); ++i) { if (i) cerr << ", "; cerr << (a)[i]; } cerr << "]" << endl; } while(0)
+#define debugline() cerr << "--------------------" << endl
+
+const int INF = 0x3f3f3f3f;
+const int NEG_INF = 0xcfcfcfcf;
+
+ll n, k;
+
+void solve() {
+    cin >> n >> k;
+    set<ll> cnt;
+    for (ll p = 1; p * p <= n; ++p) {
+        if (n % p == 0) {
+            cnt.insert(p);
+            cnt.insert(n / p);
+        }
+    }
+    // debug(cnt);
+    if (sz(cnt) < k) {
+        cout << -1 << endl;
+        return;
+    }
+    auto it = cnt.begin();
+    for (int i = 0; i < k - 1; ++i) {
+        ++it;
+    }
+    cout << *it << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    solve();
+}
