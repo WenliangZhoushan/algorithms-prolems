8ce5e70e39a520df6addffaa05068f5bfdf85e5b
亮亮 <wmz5132@psu.edu>
2026-03-04 16:16:36 -0500

    update code

---
+++ luogu/P4279.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << "{"; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << "}"; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define debug_arr(a, n) do { cerr << #a << " = ["; for (int i = 0; i < (n); ++i) { if (i) cerr << ", "; cerr << (a)[i]; } cerr << "]" << endl; } while(0)
+#define line() cerr << "--------------------" << endl
+
+const int INF = 0x3f3f3f3f;
+const int NEG_INF = 0xcfcfcfcf;
+
+int T, N;
+
+void solve() {
+    cin >> N;
+    int s = 0, c = 0, x;
+    for (int i = 0; i < N; ++i) {
+        cin >> x;
+        s ^= x;
+        c += x;
+    }
+    if (c == N) {
+        cout << (c % 2 == 0 ? "John" : "Brother") << endl;
+    } else {
+        cout << (s == 0 ? "Brother" : "John") << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> T;
+    while (T--) {
+        solve();
+    }
+}
