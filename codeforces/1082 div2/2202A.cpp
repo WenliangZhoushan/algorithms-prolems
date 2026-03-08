edb08b0b1215b5f5cb0ef527801d1d6c957b7fe7
亮亮 <wmz5132@psu.edu>
2026-03-07 22:09:37 -0500

    update code

---
+++ codeforces/1082 div2/2202A.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << "{"; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << "}"; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define debug_arr(a, n) do { cerr << #a << " = ["; for (int i = 0; i < (n); ++i) { if (i) cerr << ", "; cerr << (a)[i]; } cerr << "]" << endl; } while(0)
+#define line() cerr << "--------------------" << endl
+
+const int INF = 0x3f3f3f3f;
+const int NEG_INF = 0xcfcfcfcf;
+
+int t, x, y;
+
+void solve() {
+    cin >> x >> y;
+    if ((x + y) % 3 == 0 && -x / 4 <= y && y <= x / 2) {
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
---
+++ codeforces/1082 div2/2202B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << "{"; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << "}"; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define debug_arr(a, n) do { cerr << #a << " = ["; for (int i = 0; i < (n); ++i) { if (i) cerr << ", "; cerr << (a)[i]; } cerr << "]" << endl; } while(0)
+#define line() cerr << "--------------------" << endl
+
+const int INF = 0x3f3f3f3f;
+const int NEG_INF = 0xcfcfcfcf;
+
+int t, n;
+string X;
+
+int check(string s) {
+    int m = s.size();
+    vector<int> cnt(2);
+    for (int i = 0; i < m; i += 2) {
+        if (s.substr(i, 2) == "aa" || s.substr(i, 2) == "bb") {
+            return 0;
+        }
+        for (char& c : s.substr(i, 2)) {
+            if (c == '?') continue;
+            ++cnt[c - 'a'];
+        }
+    }
+    for (int& c : cnt) {
+        if (c > m / 2) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void solve() {
+    cin >> n;
+    cin >> X;
+    if (n % 2 == 0) {
+        cout << (check(X) ? "YES" : "NO") << endl;
+    } else {
+        bool ok = (X[0] == 'a' || X[0] == '?');
+        if (ok && check(X.substr(1, n - 1))) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+}
