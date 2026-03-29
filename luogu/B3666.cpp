ee65527b1a05027d75e676c694f017032484b035
亮亮 <wmz5132@psu.edu>
2026-03-29 14:02:10 -0400

    update code

---
+++ luogu/B3666.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+#define endl "\n"
+#define fi first
+#define se second
+#define all(x) (x).begin(), (x).end()
+#define sz(x) (int)(x).size()
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> ostream& operator<<(ostream& os, const T_container& v) { os << "{"; string sep; for (const T &x : v) os << sep << x, sep = ", "; return os << "}"; }
+#define debug(x) cerr << #x << " = " << x << endl
+#define debug2(x, y) cerr << #x << " = " << x << ", " << #y << " = " << y << endl
+#define debug3(x, y, z) cerr << #x << " = " << x << ", " << #y << " = " << y << ", " << #z << " = " << z << endl
+#define debug_arr(first, last) do { cerr << "["; bool _f = false; for (auto _it = (first); _it != (last); ++_it) { if (_f) cerr << ", "; cerr << *_it; _f = true; } cerr << "]" << endl; } while(0)
+#define debugline() cerr << "--------------------" << endl
+
+const int INF = 0x3f3f3f3f;
+const int NEG_INF = 0xcfcfcfcf;
+
+int n;
+
+void solve() {
+  cin >> n;
+  ull ans = 0;
+  vector<int> st;
+  vector<ull> nums(n + 1);
+  for (int i = 1; i <= n; ++i) {
+    cin >> nums[i];
+    while (!st.empty() && nums[i] > nums[st.back()]) {
+      ans ^= st.back();
+      st.pop_back();
+    }
+    st.push_back(i);
+    ans ^= i;
+    cout << ans << endl;
+  }
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  solve();
+}
